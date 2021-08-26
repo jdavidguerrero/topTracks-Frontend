e31e63085a97716164a3d85f29e9a386979a3509
Juan David Guerrero Castro <juan@parqco.com>
2021-08-25 22:27:00 -0500

    fix path

--- src/components/dashboard/cart.jsx
+++ src/components/dashboard/cart.jsx
@@ -53,7 +53,7 @@ class Cart extends Component {
 										<tr>
 											<td>
 												<div class="cart__img">
-													<img src={require('./src/assets/img/covers/calle85.jpeg')} alt=""/>
+													<img src={require('/../../assets/img/covers/calle85.jpeg')} alt=""/>
 												</div>
 											</td>
 											<td><a href="product.html">Calle 85 - Tiago Santo</a></td>
