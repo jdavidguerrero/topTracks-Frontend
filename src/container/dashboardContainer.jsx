046e13d220d228794af0685e749ec7370f0651f5
Sebastian Aranda <sabnq@hotmail.com>
2021-09-24 00:37:01 -0500

    logout pages

--- src/container/dashboardContainer.jsx
+++ src/container/dashboardContainer.jsx
@@ -3,23 +3,16 @@ import { BrowserRouter, Route, withRouter, Link, Switch} from 'react-router-dom'
 import logo  from './../assets/img/toptracks_logo.png'
 import RouteWithSubRoutes from '../routes/routewithSubRoutes';
 
-
 import Navbar from '../components/dashboard/navbar';
 import Sidebar from '../components/dashboard/sidebar';
 import Footer from '../components/dashboard/footer';
 import Cart from '../components/dashboard/cart';
 import Profile from '../components/dashboard/profile';
-class DashboardContainer extends Component {
 
+class DashboardContainer extends Component {
     constructor(props){
         super(props);
-        this.state = {
-         
-        };  
-        
-
     }
-
 
     render(){
         const { path } = this.props.match;
@@ -28,9 +21,8 @@ class DashboardContainer extends Component {
             <Navbar></Navbar>
             <Sidebar></Sidebar>
             <Switch>
-            <Route exact path={`${path}`}  render={props => <Cart {...props }/>} />
-            <Route exact path={`${path}/profile`}  render={props => <Profile {...props }/>} />
-              
+                <Route exact path={`${path}`}  render={props => <Cart {...props }/>} />
+                <Route exact path={`${path}/profile`}  render={props => <Profile {...props }/>} />
             </Switch>
             <Footer></Footer>
         </>
---
+++ src/pages/Logout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Redirect } from 'react-router';
+
+const Logout = () => {
+    localStorage.removeItem('user');
+    return (
+        <Redirect to='/' />
+    )
+}
+
+export default Logout;
