762f3968a2066c67df08de1a2d59c6a72c7ca7b4
Sebastian Aranda <sabnq@hotmail.com>
2021-10-04 20:38:22 -0500

    Change URL backend

--- src/api/host.js
+++ src/api/host.js
@@ -1,2 +1,2 @@
-// export const backURL = 'https://api.toptracks.co/api/v1'
-export const backURL = 'https://d42a-186-155-167-213.ngrok.io/api/v1'
+export const backURL = 'https://api.toptracks.co/api/v1'
+// export const backURL = 'https://d42a-186-155-167-213.ngrok.io/api/v1'
