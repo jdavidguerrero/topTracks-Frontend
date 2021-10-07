71a53a4e905a0fbbb62ecf954031ded767ca0039
Sebastian Aranda <sabnq@hotmail.com>
2021-10-06 22:25:24 -0500

    change host url

--- src/api/host.js
+++ src/api/host.js
@@ -1,2 +1,2 @@
 // export const backURL = 'https://api.toptracks.co/api/v1'
-export const backURL = 'http://3aff-186-155-167-213.ngrok.io'
+export const backURL = 'http://3aff-186-155-167-213.ngrok.io/api/v1'
